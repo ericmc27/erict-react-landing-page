8f58734d0f678394d9b8a6c83e98c97f36a27c30
Eric Castillo <erictomas27@hotmail.com>
2024-10-21 02:44:27 +0000

    changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,18 @@
 import React from "react";
+import NavBar from './navbar.jsx'
+import JumboTron from "./jumbotron.jsx";
+import '../../styles/index.css'
+import bootstrap from 'bootstrap'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<NavBar />
+			<JumboTron/>
+		</>
+		
 	);
 };
 
---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function addActive(element) {
+	const links = document.querySelectorAll(".nav-link");
+	links.forEach(link => { link.classList.remove('active') })
+
+	element.classList.add('active');
+}
+
+const NavBar = () => {
+	return (
+		<>
+			<nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+				<div className="container-fluid">
+					<a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarNav">
+						<ul className="navbar-nav ms-auto">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#" onClick={(e)=>addActive(e.target)}>Home</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#" onClick={(e)=>addActive(e.target)}>About</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#" onClick={(e)=>addActive(e.target)}>Services</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#" onClick={(e)=>addActive(e.target)}>Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	)
+}
+
+export default NavBar;
